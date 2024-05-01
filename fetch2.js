461232977d9542f3ce5809e6f338458a0f176621
Landon Brady <cbrady01@uafs.edu>
2024-05-01 10:12:14 -0500

    update

--- fetch2.js
+++ fetch2.js
@@ -17,7 +17,7 @@ async function refreshTable() {
     try {
         const response = await fetch("http://localhost/cs2033/singlePageApp/start.php?action=userListAPI");
         const data = await response.json();
-        
+
         const userTable = document.getElementById("userTable").getElementsByTagName('tbody')[0];
         //** Clear existing rows and Populate with New Data
         userTable.innerHTML = '';
